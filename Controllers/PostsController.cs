38dd138596d14082aca431df1591215a88d533b6
Samuele Resca <samuele.resca@gmail.com>
2016-09-22 23:10:35 +0200

    Read Only

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -15,47 +15,22 @@ namespace Blog.DotNetCoreMongoDb.Controllers
 
         protected static IMongoClient _client;
         protected static IMongoDatabase _database;
-
+        protected IMongoCollection<PostModel> _collection;
 
         public PostsController()
         {
             _client = new MongoClient();
             _database = _client.GetDatabase("demoDb");
+            _collection = _database.GetCollection<PostModel>("posts");
         }
 
-        // GET api/values
+        // GET api/posts
         [HttpGet]
         public List<PostModel> Get(string jsonQuery = "{}")
         {
-            var collection = _database.GetCollection<PostModel>("posts");            
             var queryDoc = new QueryDocument(BsonSerializer.Deserialize<BsonDocument>(jsonQuery));
             Console.WriteLine(queryDoc);
-            return collection.Find<PostModel>(queryDoc).ToList();
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return _collection.Find<PostModel>(queryDoc).ToList();
         }
     }
 }

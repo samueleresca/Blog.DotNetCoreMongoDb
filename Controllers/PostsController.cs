02a180dd00bb06c615671e44f8bbca772a2cb7d8
Samuele Resca <samuele.resca@gmail.com>
2016-09-22 21:46:49 +0200

    Fix querying + naming

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -4,31 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace Blog.DotNetCoreMongoDb.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class PostsController : Controller
     {
 
         protected static IMongoClient _client;
         protected static IMongoDatabase _database;
 
 
-        public ValuesController()
+        public PostsController()
         {
             _client = new MongoClient();
             _database = _client.GetDatabase("demoDb");
-
         }
 
         // GET api/values
         [HttpGet]
-        public  List<PostModel> Get(PostModel filter)
+        public List<PostModel> Get(string jsonQuery = "{}")
         {
-            var collection= _database.GetCollection<PostModel>("posts");
-           return  collection.Find<PostModel>(filter).ToList();
+            var collection = _database.GetCollection<PostModel>("posts");            
+            var queryDoc = new QueryDocument(BsonSerializer.Deserialize<BsonDocument>(jsonQuery));
+            Console.WriteLine(queryDoc);
+            return collection.Find<PostModel>(queryDoc).ToList();
         }
 
         // GET api/values/5
